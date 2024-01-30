e50171f67b2b8fea05b94c9bebb5c8b0915bd1ca
adrn-mm <adrian.m.muhammad@gmail.com>
2024-01-30 10:11:16 +0700

    change the password

--- code/generate_keys.py
+++ code/generate_keys.py
@@ -6,7 +6,7 @@ import streamlit_authenticator as stauth
 
 names = ["admin"]
 usernames = ["admin"]
-passwords = ["admin"]
+passwords = ["insert_your_password_here"] # change the password here
 
 hashed_passwords = stauth.Hasher(passwords).generate()
 
